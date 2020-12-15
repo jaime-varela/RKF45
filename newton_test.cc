a6c048cab61507b46631b8e221f4053356fd714e
jaime-varela <varela.jaime01@gmail.com>
2020-12-15 01:36:41 -0500

    changed to work with any container

--- newton_test.cc
+++ newton_test.cc
@@ -7,7 +7,7 @@
 // Newtonion force.
 // all quantities in SI units.
 const double alpha = 1.32754125e20;
-double Force3D(int i, double t, const std::valarray<double> & xv){
+double Force3D(int i, double t, const std::vector<double> & xv){
   double r;
   r = sqrt(xv[0]*xv[0]+xv[1]*xv[1]+xv[2]*xv[2]); 
   switch(i) {
@@ -32,13 +32,13 @@ int main(){
   double AU = 1.496e11; // astronomical unit
   double V = 30000.0; // initial velocity
   // initial conditions
-  std::valarray<double> y0 = {0.0,AU,0.0,-V,0.0,0.0}; 
+  std::vector<double> y0 = {0.0,AU,0.0,-V,0.0,0.0}; 
 
   double T = 7.2e6;
   double hi = 7.2e3/3;
   // solve the RK45 system with
   // t_0 = 0, t_f = T, y0, err = 1e05, h_i = 7.2e/3
-  std::valarray<double> result(6);  
+  std::vector<double> result(6);  
 
   RungeKutta::RK45 Newton(3*2,Force3D); 
 
@@ -50,8 +50,8 @@ int main(){
   std::cout << duration << std::endl;
 
   // Print solution y_0(tf), y_1(tf) ...
-  for(int i = 0; i < result.size(); i++)
-    std::cout << result[i] << ",";  
+  for(auto&& value : result)
+    std::cout << value << ",";
   std::cout << std::endl;
 
   return 0;
--- rkf45.h
+++ rkf45.h
@@ -6,7 +6,6 @@
 #include <algorithm>
 #include <functional>
 #include <vector>
-#include <valarray>
 
 /*
   TODO: 
@@ -14,6 +13,7 @@
     1. Fused multiply adds
     2. constant handling
     3. think about implementation
+    4. maybe use algorithms such as std::transform
 
     https://en.cppreference.com/w/cpp/iterator#C.2B.2B20_iterator_concepts
 
@@ -24,47 +24,49 @@
 
 //CONSTANTS; TODO: make tables out of these humbers
 //TODO: think about if you want type info
-double k21 = 1.0/4.0, k2t = 1.0/4.0;
-double k31 = 3.0/32.0,k32 = 9.0/32.0, k3t = 3.0/8.0;
-double k41 = 1932.0/2197.0,k42 = -7200.0/2197.0,k43 = 7296.0/2197.0, k4t = 12.0/13.0;
-double k51 = 439.0/216.0,k52 = -8.0, k53 = 3680.0/513.0, k54 = -845.0/4104.0, k5t = 1.0;
-double k61 = -8.0/27.0,k62 = 2.0,k63 = -3544.0/2565.0,k64 = 1859.0/4104.0,k65 = -11.0/40.0,k6t = 0.5;
+static double k21 = 1.0/4.0, k2t = 1.0/4.0;
+static double k31 = 3.0/32.0,k32 = 9.0/32.0, k3t = 3.0/8.0;
+static double k41 = 1932.0/2197.0,k42 = -7200.0/2197.0,k43 = 7296.0/2197.0, k4t = 12.0/13.0;
+static double k51 = 439.0/216.0,k52 = -8.0, k53 = 3680.0/513.0, k54 = -845.0/4104.0, k5t = 1.0;
+static double k61 = -8.0/27.0,k62 = 2.0,k63 = -3544.0/2565.0,k64 = 1859.0/4104.0,k65 = -11.0/40.0,k6t = 0.5;
 
 
-double y41 = 25.0/216.0;
-double y43 = 1408.0/2565.0;
-double y44 = 2197.0/4104.0;
-double y45 = -1.0/5.0;
+static double y41 = 25.0/216.0;
+static double y43 = 1408.0/2565.0;
+static double y44 = 2197.0/4104.0;
+static double y45 = -1.0/5.0;
 
 
-double y51 = 16.0/135.0;
-double y53 = 6656.0/12825.0;
-double y54 = 28561.0/56430.0;
-double y55 = -9.0/50.0;
-double y56 = 2.0/55.0;
+static double y51 = 16.0/135.0;
+static double y53 = 6656.0/12825.0;
+static double y54 = 28561.0/56430.0;
+static double y55 = -9.0/50.0;
+static double y56 = 2.0/55.0;
   
-//error calculation function
-template<class NumT = double,class CoordinateContainer = std::valarray<NumT>>
-NumT err_norm(const CoordinateContainer& a)		
-{
-  double result =0;
-  for(int i = 0; i < a.size();i++)
-    {
-      result += (a[i])*(a[i]);
-    }
-  return sqrt(result);
-}
 
 namespace RungeKutta
 {
+
+  //error calculation function
+  template<class NumT = double,class CoordinateContainer = std::vector<NumT>>
+  NumT err_norm(const CoordinateContainer& a)		
+  {
+    NumT result =0;
+    for(int i = 0; i < a.size();i++)
+      {
+        result += (a[i])*(a[i]);
+      }
+    return sqrt(result);
+  }
+
   template<class NumT,class Index = int, class CoordinateContainer = std::vector<NumT>>
   class RK45
   {
     public:
-      RK45(Index dimSize,NumT(*F_i)(Index,NumT,const CoordinateContainer &))
+      RK45(Index numEqns,NumT(*F_i)(Index,NumT,const CoordinateContainer &))
       {
         fDeriv = F_i;
-        fDimSize = dimSize;
+        fDimSize = numEqns;
       }
 
       Index getSize() const
@@ -74,7 +76,7 @@ namespace RungeKutta
 
       CoordinateContainer driver(NumT t0,NumT tf,const CoordinateContainer & y0,NumT err,NumT hi)
       {
-        Index DIM = y0.size();
+        Index DIM = fDimSize;
         NumT errestimate,errortol;
         NumT tstep = t0;
         NumT Tstep;
@@ -87,32 +89,51 @@ namespace RungeKutta
           tstep += h;
           Tstep = tstep-h;//function eval time is h-step away from tstep.
           Fvec(yf,Tstep,h,k1);
-          YVALS = yf + k21*k1;
+          for(Index i = 0;i < fDimSize;++i)
+          {
+            YVALS[i] = yf[i] + k21*k1[i];
+          }
           Fvec(YVALS, Tstep + k2t*h,h,k2);
-          YVALS = yf + k31*k1 + k32*k2;
+          for(Index i = 0;i < fDimSize;++i)
+          {
+            YVALS[i] = yf[i] + k31*k1[i] + k32*k2[i];
+          }
           Fvec(YVALS,Tstep + k3t*h,h,k3);
-          YVALS = yf + k41*k1 + k42*k2 + k43*k3;
+          for(Index i = 0;i < fDimSize;++i)
+          {
+            YVALS[i] = yf[i] + k41*k1[i] + k42*k2[i] + k43*k3[i];
+          }
           Fvec(YVALS,Tstep + k4t*h,h,k4);
-          YVALS = yf + k51*k1 + k52*k2 + k53*k3 + k54*k4;
+          for(Index i = 0;i < fDimSize;++i)
+          {
+            YVALS[i] = yf[i] + k51*k1[i] + k52*k2[i] + k53*k3[i] + k54*k4[i];
+          }
           Fvec(YVALS,Tstep + k5t*h,h,k5);
-          YVALS = yf + k61*k1 + k62*k2 + k63*k3 + k64*k4 + k65*k5;
+          for(Index i = 0;i < fDimSize;++i)
+          {
+            YVALS[i] = yf[i] + k61*k1[i] + k62*k2[i] + k63*k3[i] + k64*k4[i] + k65*k5[i];
+          }
           Fvec(YVALS,Tstep + k6t*h,h,k6);
-
-          yf = yf + (y41)*k1 + (y43)*k3 + (y44)*k4 + (y45)*k5;
-          erres=  (y51-y41)*k1 +
-	        (y53-y43)*k3 +
-	        (y54-y44)*k4 +
-	        (y55-y45)*k5 +
-	        (y56)*k6;
+          for(Index i = 0;i < fDimSize;++i)
+          {
+            yf[i] = yf[i] + (y41)*k1[i] + (y43)*k3[i] + (y44)*k4[i] + (y45)*k5[i];
+            erres[i]=  (y51-y41)*k1[i] +
+  	        (y53-y43)*k3[i] +
+  	        (y54-y44)*k4[i] +
+  	        (y55-y45)*k5[i] +
+  	        (y56)*k6[i];
+          }
 
           errestimate = err_norm(erres);
-          //      errortol = err*err_norm(yf); // relative error
           errortol = err;
           if(errestimate/errortol > 1)
       	  {
 	          tstep -= h;
 	          h *=0.9*pow(errortol/errestimate,0.2);
-	          yf = yf - (y41)*k1 - (y43)*k3 - (y44)*k4 - (y45)*k5;
+            for(Index i = 0;i < fDimSize;++i)
+            {
+  	          yf[i] = yf[i] - (y41)*k1[i] - (y43)*k3[i] - (y44)*k4[i] - (y45)*k5[i];
+            }
 	        }
           else
           {
@@ -123,10 +144,10 @@ namespace RungeKutta
           {
       	    h = tf - tstep;
           }
-        } while(tstep < tf);  
+        } while(tstep < tf);  //TODO add a max_iter to avoid infinite loop
 
       return yf;  
-    } 
+    }
 
     private:
       std::function<NumT(Index,NumT,const CoordinateContainer&)> fDeriv;
@@ -137,7 +158,9 @@ namespace RungeKutta
       void Fvec(const CoordinateContainer& yvec, NumT T,NumT hv, CoordinateContainer& k) const
       {
         for(Index i = 0; i< fDimSize;i++)
+        {
           k[i] = hv*(fDeriv(i,T,yvec) );
+        }
       }
   };
 
